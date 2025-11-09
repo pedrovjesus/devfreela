cffad6b48295fd076a48a2085207f670a927a90d
pedrovjesus <pedroferreira161jesus@gmail.com>
2025-11-09 15:20:25 -0300

    feat: created project entity (#02)

---
+++ devfreela.Core/Entities/Project.cs
@@ -0,0 +1,27 @@
+namespace devfreela.Core.entities
+{
+    public class Project : BaseEntity
+    {
+        public Project(string title, string description, int idClient, decimal totalCost)
+        {
+            Title = title;
+            Description = description;
+            IdClient = idClient;
+            TotalCost = totalCost;
+
+            CreatedAt = DateTime.Now;
+            Status = ProjectStatusEnum.Created;
+            Comments = new List<ProjectComment>();
+        }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public int IdClient { get; private set; }
+        public int IdFreelancer { get; private set; }
+        public decimal TotalCost { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? StartedAt { get; private set; }
+        public DateTime? FinishedAt { get; private set; }
+        public ProjectStatusEnum Status { get; private set; }
+        public list<ProjectComment> Comments { get; private set; }
+    }
+}
