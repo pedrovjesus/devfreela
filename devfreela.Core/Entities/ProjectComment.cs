73bb31bf3c42e02438c80994d5bf3c44f279885b
pedrovjesus <pedroferreira161jesus@gmail.com>
2025-11-09 15:30:01 -0300

    feat: created projectComment entity (#04)

---
+++ devfreela.Core/Entities/ProjectComment.cs
@@ -0,0 +1,18 @@
+namespace devfreela.Core.entities
+{
+    public class ProjectComment
+    {
+        public ProjectComment(string content, int idProject, int idUser)
+        {
+            Content = content;
+            IdProject = idProject;
+            IdUser = idUser;
+            CreatedAt = DateTime.Now;
+        }
+        public string Content { get; private set; }
+        public int IdProject { get; private set; }
+        public int IdUser { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+    }
+}
